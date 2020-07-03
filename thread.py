da237abc4e4816be608df0c75c946335ce197904
AikoSakakibara <67773542+AikoSakakibara@users.noreply.github.com>
2020-07-03 14:34:14 +0900

    Update thread.py

--- thread.py
+++ thread.py
@@ -1,24 +1,28 @@
-from threading import Thread
+from threading import Thread, Semaphore
 
 def my_function1():
+  with semaphore:
   for i in range(10000):
     print("123")
     print("456")
     print("789")
 
 def my_function2():
+  with semaphore:
   for i in range(10000):
     print("abc")
     print("def")
     print("ghi")
 
 def my_function3():
+  with semaphore:
   for i in range(10000):
     print("ABC")
     print("DEF")
     print("GHI")
 
 if __name__ == "__main__":
+  semaphore=Semaphore(1)
   t1 = Thread(target = my_function1)
   t2 = Thread(target = my_function2)
   t3 = Thread(target = my_function3)
